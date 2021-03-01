b06d14ff3a2fc76174bce1d8ab06b0eef60df389
repl.it user <replituser@example.com>
2021-03-01 14:26:46 +0000

    coding is life

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#Displaying escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a backslash")
+print("\" Displays double quote")
+print(" \"Displays single quote")
