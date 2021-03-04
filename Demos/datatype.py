00a27f3982485694ad814b236ece15fde001983c
repl.it user <replituser@example.com>
2021-03-04 18:31:20 +0000

    If statement

--- Demos/datatype.py
+++ Demos/datatype.py
@@ -1,10 +1,15 @@
 print("What's your name?")
 #Variable is a container, which can store date for us in the memory such as string,integer,float,boolean)
 name =input()
+#input is used to get the data from the user
+
 print ("What's your age?")
 age = int (input())
+#Casting is used to specify variable
+
 print("What's your bank balance?")
 balance = float (input())
 
 print ("Welcome {},You are {} years old,Your bank balance is {:.2f}.".format (name,age,balance))
+# .format is used to pull the data of variable
 #:.2f is used to display two decimal number after 
