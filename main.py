ca903a7ee62735b81a0a090df1c9baef5c92adcd
repl.it user <replituser@example.com>
2021-03-05 11:26:28 +0000

    Added code to read user input

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# Display a box
-print ("##########")
-print ("#        #")
-print ("#        #")
-print ("##########")
+#Ask user to enter their name
+
+print ("What is your name human?")
+name = input()
+print ("It's nice to meet you human " + name + ".")
+# + operator combines the string without adding a space
+# format function can be used to display the same message
+# format function uses placeholders {} and then replaces these with the supplied values
+#it can be rewritten as follows
+print("Nice to meet you {}." .format (name))
