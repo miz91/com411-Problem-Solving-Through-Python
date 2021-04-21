b88ec363b20ff81f173209b490cdc1a5b7b2013a
repl.it user <replituser@example.com>
2021-04-21 23:13:43 +0000

    Use of multiple nested statement

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
-#Asking user for input
-print("Enter the cover type of the book. hard or soft?")
-cover = input()
+print("Where should i look?")
+response = input()
 
-if (cover == "soft"):
-  print("Is the book perfect bound?")
-  perfect_bound = input()
-
-  #this will only run if the above condition is true
-
-  if(perfect_bound == "yes"):
-    print("Soft cover, perfect bound books are very popular!")
+if (response == "in the bedroom"):
+  print("Where in the bedroom should i look?")
+  location = input()
+  if (location == "under the bed"):
+    print("Found some shoes but no battery")
   else:
-    print("Soft covers with coils or stitches are great for short books")
+   print("Found some mess but no battery")
+
+elif (response == "in the bathroom"):
+  print("Where in the bathroom should i look?")
+  location = input()
+  if(location == "in the bathtub"):
+    print("Found a rubber duck but no battery")
+  else:
+    print("Found a wet surface but no battery")
+
+elif (response == "in the lab"):
+  print("Where in the lab should i look?")
+  location = input ()
+  if (location == "on the table"):
+    print("Yes! I found my battery")
+  else:
+    print("Found some tools but no battery")
 
 else:
-  print("Books with hard covers can be more expensive!")
+  print("I do not know where that is but I will keep looking.")
+
+
+# #Asking user for input
+# print("Enter the cover type of the book. hard or soft?")
+# cover = input()
+
+# if (cover == "soft"):
+#   print("Is the book perfect bound?")
+#   perfect_bound = input()
+
+#   #this will only run if the above condition is true
+
+#   if(perfect_bound == "yes"):
+#     print("Soft cover, perfect bound books are very popular!")
+#   else:
+#     print("Soft covers with coils or stitches are great for short books")
+
+# else:
+#   print("Books with hard covers can be more expensive!")
