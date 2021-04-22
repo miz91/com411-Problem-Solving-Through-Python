68511c65d27e0886a8d3134a88652315ec1e9558
repl.it user <replituser@example.com>
2021-04-22 12:07:50 +0000

    Use of a logical operator

--- basics/decision/nested decision/nestceptio.py
+++ basics/decision/nested decision/nestceptio.py
@@ -1,37 +1,48 @@
-# Ask user for place
-print("Where should I look?")
-place = input()
+print("Where should i look?")
+response = input()
 
-# Check the bedroom
-if (place == "in the bedroom"):
-    print("Where in the bedroom should I look?")
-    bedroom_place = input()
+if (response == "in the bedroom"):
+  print("Where in the bedroom should i look?")
+  location = input()
+  if (location == "under the bed"):
+    print("Found some shoes but no battery")
+  else:
+   print("Found some mess but no battery")
 
-    if (bedroom_place == "under the bed"):
-        print("Found some shoes but no battery")
-    else:
-        print("Found some mess but no battery.") 
+elif (response == "in the bathroom"):
+  print("Where in the bathroom should i look?")
+  location = input()
+  if(location == "in the bathtub"):
+    print("Found a rubber duck but no battery")
+  else:
+    print("Found a wet surface but no battery")
 
-# Check the bathroom
-elif (place == "in the bathroom"):
-    print("Where in the bathroom should I look?")
-    bathroom_place = input()
+elif (response == "in the lab"):
+  print("Where in the lab should i look?")
+  location = input ()
+  if (location == "on the table"):
+    print("Yes! I found my battery")
+  else:
+    print("Found some tools but no battery")
 
-    if (bathroom_place == "in the bathtub"):
-        print("Found a rubber duck but no battery")
-    else:
-        print("Found a wet surface but no battery.")
-
-# Check the lab
-elif (place == "in the lab"):
-    print("Where in the lab should I look?")
-    lab_place = input()
-
-    if (lab_place == "on the table"):
-        print("Yes! I found my battery!")
-    else:
-        print("Found some tools but no battery.")
-
-# Handle unknown place
 else:
-    print("I am not sure where that place is located.")
+  print("I do not know where that is but I will keep looking.")
+
+
+# #Asking user for input
+# print("Enter the cover type of the book. hard or soft?")
+# cover = input()
+
+# if (cover == "soft"):
+#   print("Is the book perfect bound?")
+#   perfect_bound = input()
+
+#   #this will only run if the above condition is true
+
+#   if(perfect_bound == "yes"):
+#     print("Soft cover, perfect bound books are very popular!")
+#   else:
+#     print("Soft covers with coils or stitches are great for short books")
+
+# else:
+#   print("Books with hard covers can be more expensive!")
