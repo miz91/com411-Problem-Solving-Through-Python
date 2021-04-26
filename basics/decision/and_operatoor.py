9fad8835a83e5a4c3897526efa44842677c36bc6
repl.it user <replituser@example.com>
2021-04-26 13:57:27 +0000

    While loops

--- basics/decision/and_operatoor.py
+++ basics/decision/and_operatoor.py
@@ -2,7 +2,7 @@ print("What did i hear?")
 answer1 = input()
 print("What did i see?")
 answer2 = input()
-
+#program will run if both is true
 if((answer1 == "grr") and (answer2 == "two red eyes")):
   print("There is a scary creature. I should get of here!")
 
